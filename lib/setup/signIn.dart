47b66bd6924dfc909c180773c91c4901e679641c
zaenury <adhiimzaenury@gmail.com>
2021-03-28 10:10:09 +0800

    login and integration firebase

---
+++ lib/setup/signIn.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String _email, _password;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sign in"),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              validator: (input) {
+                if (input.isEmpty) {
+                  return 'email cannot be empty';
+                }
+                return null;
+              },
+              onSaved: (input) => _email = input,
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            TextFormField(
+              validator: (input) {
+                if (input.length < 8) {
+                  return 'password needs to be at least 8 characters';
+                }
+                return null;
+              },
+              onSaved: (input) => _password = input,
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            ElevatedButton(onPressed: () {}, child: Text('Sign In'))
+          ],
+        ),
+      ),
+    );
+  }
+
+  void signIn() {
+    final formState = _formKey.currentState;
+    if (formState.validate()) {}
+  }
+}
